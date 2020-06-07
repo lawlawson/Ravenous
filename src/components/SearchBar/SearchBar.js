dae26919d350b942f7e1ffafb416bfb428b30a19
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2020-06-07 14:44:05 +0100

    Fixed SearchBar.js code structure and App.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,29 +8,32 @@ const sortByOptions = {
 }
 
 class SearchBar extends React.Component {
+    renderSortbyOptions() {
+        return Object.keys(sortByOptions).map(sortByOption => {
+            let sortByOptionValue = sortByOptions[sortByOption];
+               return <li key="sortByOptionValue">{sortByOption}</li>
+        });  
+    }
+
     render() {
         return(
             <div className="SearchBar">
-            <div className="SearchBar-sort-options">
-                <ul>
-                    renderSortByOptions();
-                </ul>
-            </div>
-            <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
-            </div>
-            <div className="SearchBar-submit">
-                <a>Let's Go</a>
-            </div>
-            </div>
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a href="www.#.com">Let's Go</a>
+                </div>
+            </div> 
+        
         );
-        renderSortbyOptions() {
-            return Object.keys(sortByOptions).map(sortByOption => {
-                let sortByOptionValue = sortByOptions[sortByOption];
-            <li key="sortByOptionValue">{sortByOption}</li>
-            });  
-        }
+        
     } 
          
 }
