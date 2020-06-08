0b7300fc1c494852644ba16d66bfe7875c5f3725
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2020-06-08 23:09:44 +0100

    Completed passing information to components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,13 +5,12 @@ import Business from '../Business/business';
 class BusinessList extends React.Component {
     render () {
         return (
-            <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+            <div className="BusinessList"> 
+            {
+                this.props.businesses.map((business) => {
+                    return <Business business={business}/>;
+                })
+            }
             </div>
         );
     }
