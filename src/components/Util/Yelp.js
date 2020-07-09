9e83b4a62008e6cefb6c02a6b907ff870dd81d72
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2020-07-09 17:08:01 +0100

    Completed yelp component

--- src/components/Util/Yelp.js
+++ src/components/Util/Yelp.js
@@ -15,10 +15,21 @@ const yelp = {
           if(jsonResponse.businesses) {
             return jsonResponse.business.map(((business) => {
                 return {
-
-                }
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count,
+                };
             }));
           }
       })
   }
 };
+
+export default yelp;
