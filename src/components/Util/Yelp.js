92dd41227e2e9a3d7ab52fef481f16f577ff3c96
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2020-07-09 15:10:33 +0100

    Created yelp object

--- src/components/Util/Yelp.js
+++ src/components/Util/Yelp.js
@@ -1,2 +1,8 @@
 const apiKey =
   "ZuCtPrATZCADwg1y4P8wwIXH98VHDcWvLkrNO7_aDloBPlSG4bBsBv6GEMZUxggCIpJbQrznag3jjz7eObYNjyN60LtmHKwCe43LtJY8zrzyQDjNeCxuOEx-CVcGX3Yx";
+
+const yelp = {
+    searchYelp(term, location, sortBy) {
+        return fetch('https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}');
+    }
+};
